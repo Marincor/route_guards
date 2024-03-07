53c4e26facc5fd10aa3851efc89692a9e19d8a62
Gabriel Claudino Marinho <gabriel.cmunivos@protonmail.com>
2024-03-07 19:10:26 -0300

    feat: include multipleGuards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { guard1, guard2, multipleGuards } from 'src/guards/auth-guard.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+   path: '', component: AppComponent,
+   canActivate: [multipleGuards([guard1, guard2])]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/guards/auth-guard.guard.ts
@@ -0,0 +1,29 @@
+import { inject, EnvironmentInjector } from "@angular/core";
+import { CanActivateFn, CanActivateChildFn, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
+import { runInInjectionContext } from '@angular/core';
+
+export function multipleGuards(guards: (CanActivateFn | CanActivateChildFn)[]) {
+    return async (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+        const injector = inject(EnvironmentInjector);
+        for (const guard of guards) {
+            const guardResult = await runInInjectionContext(injector, () => guard(route, state));
+            if (!guardResult) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+export const guard1 = async () => {
+    console.log('guard 1')
+    const resultOfFoo = await new Promise(resolve => setTimeout(() => resolve(true), 5000));
+
+    console.log("6", resultOfFoo)
+    return true;
+}
+
+export const guard2 = () => {
+    console.log('guard 2')
+    return true;
+}
